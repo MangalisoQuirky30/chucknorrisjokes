fddfa6680cd86fcbea81861640a9dcfbebf5cf4e
Mangaliso_Sereo <49154272+MangalisoQuirky30@users.noreply.github.com>
2019-10-02 15:12:08 +0200

    commit to master

---
+++ VS2019_CONSOLE/ChuckNorrisJokes/Program.cs
@@ -0,0 +1,36 @@
+﻿using ChuckNorrisJokesLibrary;
+using System.Threading.Tasks;
+
+using System;
+
+namespace ChuckNorrisJokes
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            JokeGenerator jokeGenerator = new JokeGenerator();
+            string randomJoke = await jokeGenerator.GetRandomJoke();
+
+            string[] catStrArr = await jokeGenerator.GetCategories();
+
+            Console.WriteLine(randomJoke) ;
+
+
+            for( int i = 0; i < catStrArr.Length; i++ )
+            {
+                Console.WriteLine(catStrArr[i]);
+            }
+
+
+            
+            for(int y = catStrArr.Length -1; y > 0; y--)
+            {
+                Console.WriteLine(catStrArr[y]);
+            }
+
+
+           
+        }
+    }
+}
---
+++ VS2019_CONSOLE/ChuckNorrisJokesLibrary/JokeGenerator.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisJokesLibrary
+{
+    public class JokeGenerator
+    {
+        public async Task<string> GetRandomJoke()
+        {
+            var client = new HttpClient() ;
+            string randomJoke = await client.GetStringAsync("https://api.chucknorris.io/jokes/random");
+            // this return a string found in that url... its a json text in a string format
+
+            var joke = JsonConvert.DeserializeObject<Joke>(randomJoke);
+            // this converts  the json in string form (randomJoke) to csharp cstructure specified in 'joke.ce'-class
+            // take json, turn it to csharp
+
+            return joke.value;
+        }
+
+
+        public async Task<string[]> GetCategories()
+        {
+            var categories = new HttpClient();
+            string categoriesstring = await categories.GetStringAsync("https://api.chucknorris.io/jokes/categories");
+
+            var categoriesStrArr = JsonConvert.DeserializeObject<string[]>(categoriesstring);
+            return categoriesStrArr;
+        }
+    }
+}
+
